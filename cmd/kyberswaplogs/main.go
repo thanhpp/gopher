cf765ac9001cf4530ce1eb274064b9d31aa70e41
thanhpp <thanhphanphu18@gmail.com>
2022-08-29 13:58:34 +0700

    fix: 🐛 kyberswaplogs: fix topics check
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- cmd/kyberswaplogs/main.go
+++ cmd/kyberswaplogs/main.go
@@ -11,8 +11,9 @@ import (
 )
 
 var (
-	bscURL = "https://bsc-dataseed.binance.org"
-	txHash = "0xcd76d6acce33c3b7903413eceb86548d0457a55811f48056da2f13f5c26d1245"
+	bscURL                   = "https://bsc-dataseed.binance.org"
+	txHash                   = "0xcd76d6acce33c3b7903413eceb86548d0457a55811f48056da2f13f5c26d1245"
+	kyberswapBSCSwappedTopic = "0xd6d4f5681c246c9f42c203e287975af1601f8df8035a9251f79aab5c8f09e2f8"
 )
 
 func main() {
@@ -39,12 +40,21 @@ func main() {
 	}
 
 	for i := range receipt.Logs {
-		result, err := kyberswapBSC.ParseSwapped(*receipt.Logs[i])
-		if err != nil {
-			log.Println("[SKIP ERROR] kyberswapbsc parse swapped error:", err)
+		if len(receipt.Logs[i].Topics) == 0 {
 			continue
 		}
 
-		log.Printf("kyberswapbsc parse swapped: %+v", result)
+		for j := range receipt.Logs[i].Topics {
+			if receipt.Logs[i].Topics[j].String() == kyberswapBSCSwappedTopic {
+				result, err := kyberswapBSC.ParseSwapped(*receipt.Logs[i])
+				if err != nil {
+					log.Println("[SKIP ERROR] kyberswapbsc parse swapped error:", err)
+					continue
+				}
+
+				log.Printf("reading tx receipt: %+v", receipt.Logs[i])
+				log.Printf("kyberswapbsc parse swapped: %+v", result)
+			}
+		}
 	}
 }
